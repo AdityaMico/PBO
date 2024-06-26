3d4324bf0eb94f012924e3b60b5df6ff546593b1
AdityaMico <104138920+AdityaMico@users.noreply.github.com>
2024-06-26 22:48:27 +0700

    Add files via upload

---
+++ tugas/Kalkulator.java
@@ -0,0 +1,86 @@
+Package tugas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Kalkulator {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Kalkulator Sederhana");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 2, 5, 5));
+
+        JTextField input1 = new JTextField();
+        JTextField input2 = new JTextField();
+
+        JLabel resultLabel = new JLabel("Hasil: ");
+        
+        JButton addButton = new JButton("+");
+        JButton subtractButton = new JButton("-");
+        JButton multiplyButton = new JButton("*");
+        JButton divideButton = new JButton("/");
+        JButton modButton = new JButton("mod");
+
+        panel.add(input1);
+        panel.add(input2);
+        panel.add(addButton);
+        panel.add(subtractButton);
+        panel.add(multiplyButton);
+        panel.add(divideButton);
+        panel.add(modButton);
+
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, resultLabel);
+
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Double.parseDouble(input1.getText());
+                double num2 = Double.parseDouble(input2.getText());
+                double result = num1 + num2;
+                resultLabel.setText("Hasil: " + result);
+            }
+        });
+
+        subtractButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Double.parseDouble(input1.getText());
+                double num2 = Double.parseDouble(input2.getText());
+                double result = num1 - num2;
+                resultLabel.setText("Hasil: " + result);
+            }
+        });
+
+        multiplyButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Double.parseDouble(input1.getText());
+                double num2 = Double.parseDouble(input2.getText());
+                double result = num1 * num2;
+                resultLabel.setText("Hasil: " + result);
+            }
+        });
+
+        divideButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Double.parseDouble(input1.getText());
+                double num2 = Double.parseDouble(input2.getText());
+                double result = num1 / num2;
+                resultLabel.setText("Hasil: " + result);
+            }
+        });
+
+        modButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Double.parseDouble(input1.getText());
+                double num2 = Double.parseDouble(input2.getText());
+                double result = num1 % num2;
+                resultLabel.setText("Hasil: " + result);
+            }
+        });
+
+        frame.setVisible(true);
+    }
+}
